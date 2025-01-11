a1c70a55a4cbe42c12e84387482120df8da46e46
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-11 18:54:24 +0530

    `Added new styles for Coin page and updated Coin.jsx to include additional coin information`

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -31,7 +31,7 @@ const Coin = () => {
     headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-uQKtdbWQPECxYxzki8Bb3J8a'}
   };
   
-  fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`, options)
+  fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
     .then(res => res.json())
     .then(res => setHistoricalData(res))
     .catch(err => console.error(err));
@@ -46,10 +46,33 @@ if(coinData && historicalData){
     <div className='coin'>
       <div className='coin-name'>
           <img src={coinData.image.large}/>
-          <p>{coinData.name} ({coinData.symbol.toUpperCase()})</p>
+          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+            
         </div>
         <div className='coin-chart'>
           <LineChart historicalData={historicalData}/>
+        </div>
+        <div className='coin-info'>
+          <ul>
+            <li>Crypto Market Rank</li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
+          <ul>
+            <li>Current Price</li>
+            <li>{currency.symbol}{coinData.market_data.current_price[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>Market Cap</li>
+            <li>{currency.symbol}{coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>24 Hour high</li>
+            <li>{currency.symbol}{coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>24 Hour low</li>
+            <li>{currency.symbol}{coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
+          </ul>
         </div>
     </div>
   )
